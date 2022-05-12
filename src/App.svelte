4142606303031b42194d99057bad1c9314c9b2f5
Noé <noe.philippe@bob-le-developpeur.com>
2022-05-12 12:30:06 +0200

    Debut projet

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+  import Input from './components/Input.svelte';
+  import Result from './components/Result.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Conversion App</h1>
+  <div class="container">
+    <Input />
+    <Input />
+    <Input />
+    <h2>Résultat de la convertion :</h2>
+    <Result />
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/components/Result.svelte
@@ -0,0 +1,8 @@
+<script></script>
+
+<ul>
+  <li>Kg :</li>
+  <li>gh :</li>
+</ul>
+
+<style></style>
