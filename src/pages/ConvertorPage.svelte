1e6a002ddc3773715db6d62b9c0b567686d41eb4
Noé <noe.philippe@bob-le-developpeur.com>
2022-06-01 12:53:47 +0200

    Correction conversion masse Si vers Imperial

--- src/pages/ConvertorPage.svelte
+++ src/pages/ConvertorPage.svelte
@@ -1,12 +1,12 @@
 <script>
-  import InputImperialLength from "../components/Length/InputImperialLength.svelte";
-  import InputSILength from "../components/Length/InputSILength.svelte";
-  import InputImperialWeight from "../components/Weight/InputImperialWeight.svelte";
-  import InputSIWeight from "../components/Length/InputSILength.svelte";
-  import { length_UnitsImperial, length_UnitsSI } from "../data/dataLength";
-  import { weight_UnitsImperial, weight_UnitsSI } from "../data/dataWeight";
-  import { converter } from "../utils/converter";
-  import Result from "../components/Result.svelte";
+  import InputImperialLength from '../components/Length/InputImperialLength.svelte';
+  import InputSILength from '../components/Length/InputSILength.svelte';
+  import InputImperialWeight from '../components/Weight/InputImperialWeight.svelte';
+  import InputSIWeight from '../components/Weight/InputSIWeight.svelte';
+  import { length_UnitsImperial, length_UnitsSI } from '../data/dataLength';
+  import { weight_UnitsImperial, weight_UnitsSI } from '../data/dataWeight';
+  import { converter } from '../utils/converter';
+  import Result from '../components/Result.svelte';
 
   export let whatConversion;
   export let factorConversionList;
@@ -14,24 +14,24 @@
   let enterUnits = imperialResolver(whatConversion);
   let resultUnits = siResolver(whatConversion);
   let inputValue = 0;
-  let isImperialUnit = enterUnits[0].kind == "imperial";
+  let isImperialUnit = enterUnits[0].kind == 'imperial';
 
   function imperialResolver(whatConversion) {
     switch (whatConversion) {
-      case "Longueur":
+      case 'Longueur':
         return length_UnitsImperial;
       default:
-      case "Poids":
+      case 'Poids':
         return weight_UnitsImperial;
     }
   }
 
   function siResolver(whatConversion) {
     switch (whatConversion) {
-      case "Longueur":
+      case 'Longueur':
         return length_UnitsSI;
       default:
-      case "Poids":
+      case 'Poids':
         return weight_UnitsSI;
     }
   }
@@ -48,7 +48,7 @@
 
 <!-- Bouton pour changer le système d'unité  -->
 <button class="button is-primary block" on:click={changeSystemUnit}>
-  {enterUnits[0].kind} 
+  {enterUnits[0].kind}
   <i class="p-2 fas fa-arrow-right-long" />
   {resultUnits[0].kind}
 </button>
@@ -56,12 +56,12 @@
 <div class="container block">
   <div class="is-inline-flex">
     {#if isImperialUnit}
-      {#if whatConversion == "Longueur"}
+      {#if whatConversion == 'Longueur'}
         <InputImperialLength bind:inputValue />
       {:else}
         <InputImperialWeight bind:inputValue />
       {/if}
-    {:else if whatConversion == "Longueur"}
+    {:else if whatConversion == 'Longueur'}
       <InputSILength bind:inputValue />
     {:else}
       <InputSIWeight bind:inputValue />
